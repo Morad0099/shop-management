4db58f011c84c2741f05a2bae3dc5ee2a41da7bd
Musah Morad <musahmorad01@gmail.com>
2024-11-14 22:43:54 -0500

    Update login form action to use named route for custom authentication

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RouteController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [CustomAuthController::class, 'login']);
+Route::post('/custom-login', [CustomAuthController::class, 'login'])->name('custom-login');
 
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/', [RouteController::class, 'dashboard'])->name('dashboard');
